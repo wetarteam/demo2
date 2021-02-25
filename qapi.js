55720c923a35787ed09ff8e47bd1035aac2e026f
wetarteam <78031947+wetarteam@users.noreply.github.com>
2021-02-25 16:53:51 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6037887b1a979b001ff31f87",
+				"6037887b1a979b001ff31f89"
+			]
+		},
+		"6037887b1a979b001ff31f87": {
+			"id": "6037887b1a979b001ff31f87",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6037887b1a979b001ff31f89": {
+			"id": "6037887b1a979b001ff31f89",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
